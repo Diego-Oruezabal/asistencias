95bfd2d403577b27f44033325fd2250076751ee2
Diego Oruezabal <diegooruezabal@gmail.com>
2025-10-24 20:34:59 +0200

    cambio en sInfoEmpty

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -142,7 +142,7 @@
       "sEmptyTable": "No hay datos en la Tabla",
       "sZeroRecords": "No se encontraron resultados",
       "sInfo": "Mostrando registros del _START_ al _END_ de un total _TOTAL_",
-      "SInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0",
+      "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0",
       "sInfoFiltered": "(filtrando de un total de _MAX_ registros)",
       "oPaginate": {
 
